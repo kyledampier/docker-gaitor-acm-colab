44a335adac57b42814d8d7001e7d72577eade16a
Kyle Dampier <30601856+kyledampier@users.noreply.github.com>
2022-10-02 13:46:51 -0400

    added base React app to web directory

---
+++ web/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="flexbox-item flexbox-item-1">Item 1</div>
+      <div className="flexbox-item flexbox-item-2">Item 2</div>
+    </div>
+  );
+}
+
+export default App;
